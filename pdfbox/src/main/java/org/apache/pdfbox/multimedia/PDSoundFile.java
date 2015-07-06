89d83d8bff3743849d9550c0604802c48cdef0f5
Slobodan Pejic <slobo@showbie.com>
2015-07-06 22:00:28 +0000

    Implementing raw sound creation.

--- pdfbox/src/main/java/org/apache/pdfbox/multimedia/PDSoundFile.java
+++ pdfbox/src/main/java/org/apache/pdfbox/multimedia/PDSoundFile.java
@@ -1,17 +1,28 @@
 package org.apache.pdfbox.multimedia;
 
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSDictionary;
-import org.apache.pdfbox.pdmodel.common.COSObjectable;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.pdfbox.cos.COSInteger;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.cos.COSNumber;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.common.PDStream;
 import org.apache.pdfbox.pdmodel.common.filespecification.PDFileSpecification;
 
-public class PDSoundFile implements COSObjectable {
+public class PDSoundFile extends PDStream {
 
-	COSDictionary dictionary = new COSDictionary();
+	static public COSName ENCODING_RAW = COSName.getPDFName("Raw");
+	static public COSName ENCODING_SIGNED = COSName.getPDFName("Signed");
+	static public COSName ENCODING_MU_LAW = COSName.getPDFName("muLaw");
+	static public COSName ENCODING_A_LAW = COSName.getPDFName("ALaw");
 
-	@Override
-	public COSBase getCOSObject() {
-		return dictionary;
+	public PDSoundFile() {
+		super();
+	}
+
+	public PDSoundFile(PDDocument doc, InputStream str) throws IOException {
+		super(doc, str);
 	}
 
 	/**
@@ -19,7 +30,23 @@ public class PDSoundFile implements COSObjectable {
 	 * @param file
 	 */
 	public void setFile(PDFileSpecification file) {
-		dictionary.setItem("F", file);
+		getStream().setItem(COSName.F, file);
+	}
+
+	public void setSamplingRate(COSNumber sampleRate) {
+		getStream().setItem(COSName.R, sampleRate);
+	}
+
+	public void setNumberOfSoundChannels(COSInteger channels) {
+		getStream().setItem(COSName.C, channels);
+	}
+
+	public void setBitsPerSample(COSInteger bitsPerSample) {
+		getStream().setItem(COSName.B, bitsPerSample);
+	}
+
+	public void setEncodingFormat(COSName encodingFormat) {
+		getStream().setItem(COSName.E, encodingFormat);
 	}
 
 }
